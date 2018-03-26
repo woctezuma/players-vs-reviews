938b7e3d2bb98a7359f85b94d11863fa258d014a
Wok <woctezuma@users.noreply.github.com>
2018-03-26 13:41:44 +0200

    Make sure the output is a copy-able list

--- compute_ratio_players_vs_reviews.py
+++ compute_ratio_players_vs_reviews.py
@@ -28,7 +28,8 @@ def rank_games_based_on_ratio_players_vs_reviews(steamspy_data, ratio_exponent=1
     # Get game names
     game_name_ranking = list(map(lambda appID: steamspy_data[appID]['name'], appID_ranking))
 
-    ranking = zip(appID_ranking, game_name_ranking)
+    # The following call to list() allows to copy the ranking, e.g. to compute its length before iterating over it.
+    ranking = list(zip(appID_ranking, game_name_ranking))
 
     return ranking
 
@@ -40,16 +41,13 @@ def print_ranking_to_file_stream(ranking, outfile=None, num_top_games_to_print=N
 
     base_steam_store_url = "http://store.steampowered.com/app/"
 
-    # The following line allows to copy the ranking, to compute its length before iterating over it.
-    ranking_list = list(ranking)
-
-    num_games = len(ranking_list)
+    num_games = len(ranking)
     if num_top_games_to_print is not None:
         num_games = min(num_games, num_top_games_to_print)
     rank_width = ceil(log10(num_games))
     rank_format_str = '{:0' + str(rank_width) + '}'
 
-    for (iter_no, game_info) in enumerate(ranking_list):
+    for (iter_no, game_info) in enumerate(ranking):
         current_rank = iter_no + 1
         (appid, game_name) = game_info
 
